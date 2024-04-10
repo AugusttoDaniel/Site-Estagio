e6aec9fcb348ce187e59fad567aabec43d4d3d31
AugusttoDaniel <danielsje7133@gmail.com>
2024-04-10 01:45:29 -0300

    Alterações
    Adicionando novos campos e mexendo no handleSignup

--- src/pages/Signup/index.js
+++ src/pages/Signup/index.js
@@ -6,67 +6,81 @@ import { Link, useNavigate } from "react-router-dom";
 import useAuth from "../../hooks/useAuth";
 
 const Signup = () => {
-  const [email, setEmail] = useState("");
-  const [emailConf, setEmailConf] = useState("");
-  const [senha, setSenha] = useState("");
-  const [error, setError] = useState("");
-  const navigate = useNavigate();
+	const [nome, setNome] = useState("");
+	const [email, setEmail] = useState("");
+	const [emailConf, setEmailConf] = useState("");
+	const [senha, setSenha] = useState("");
+	const [telefone, setTelefone] = useState("");
+	const [error, setError] = useState("");
+	const navigate = useNavigate();
 
-  const { signup } = useAuth();
+	const { signup } = useAuth();
 
-  const handleSignup = () => {
-    if (!email | !emailConf | !senha) {
-      setError("Preencha todos os campos");
-      return;
-    } else if (email !== emailConf) {
-      setError("Os e-mails não são iguais");
-      return;
-    }
+	const handleSignup = () => {
+		if (!nome | !email | !emailConf | !senha | !telefone) {
+			setError("Preencha todos os campos");
+			return;
+		} else if (email !== emailConf) {
+			setError("Os e-mails não são iguais");
+			return;
+		}
 
-    const res = signup(email, senha);
+		const res = signup(nome, email, senha, telefone);
 
-    if (res) {
-      setError(res);
-      return;
-    }
+		if (res) {
+			setError(res);
+			return;
+		}
 
-    alert("Usuário cadatrado com sucesso!");
-    navigate("/");
-  };
+		alert("Usuário cadatrado com sucesso!");
+		navigate("/");
+	};
 
-  return (
-    <C.Container>
-      <C.Label>Registre-Se</C.Label>
-      <C.Content>
+	return (
+		<C.Container>
+			<C.Label>Registre-Se</C.Label>
+			<C.Content>
+				<Input
+					type="string"
+					placeholder="Digite seu Nome"
+					value={nome}
+					onChange={(e) => [setNome(e.target.value), setError("")]}
+				/>
+				<Input
+					type="email"
+					placeholder="Digite seu E-mail"
+					value={email}
+					onChange={(e) => [setEmail(e.target.value), setError("")]}
+				/>
+				<Input
+					type="email"
+					placeholder="Confirme seu E-mail"
+					value={emailConf}
+					onChange={(e) => [setEmailConf(e.target.value), setError("")]}
+				/>
+				<Input
+					type="password"
+					placeholder="Digite sua Senha"
+					value={senha}
+					onChange={(e) => [setSenha(e.target.value), setError("")]}
+				/>
         <Input
-          type="email"
-          placeholder="Digite seu E-mail"
-          value={email}
-          onChange={(e) => [setEmail(e.target.value), setError("")]}
-        />
-        <Input
-          type="email"
-          placeholder="Confirme seu E-mail"
-          value={emailConf}
-          onChange={(e) => [setEmailConf(e.target.value), setError("")]}
-        />
-        <Input
-          type="password"
-          placeholder="Digite sua Senha"
-          value={senha}
-          onChange={(e) => [setSenha(e.target.value), setError("")]}
-        />
-        <C.labelError>{error}</C.labelError>
-        <Button Text="Inscrever-se" onClick={handleSignup} />
-        <C.LabelSignin>
-          Já tem uma conta?
-          <C.Strong>
-            <Link to="/">&nbsp;Entre</Link>
-          </C.Strong>
-        </C.LabelSignin>
-      </C.Content>
-    </C.Container>
-  );
+					type="string"
+					placeholder="Digite seu Telefone"
+					value={telefone}
+					onChange={(e) => [setTelefone(e.target.value), setError("")]}
+				/>
+				<C.labelError>{error}</C.labelError>
+				<Button Text="Inscrever-se" onClick={handleSignup} />
+				<C.LabelSignin>
+					Já tem uma conta?
+					<C.Strong>
+						<Link to="/">&nbsp;Entre</Link>
+					</C.Strong>
+				</C.LabelSignin>
+			</C.Content>
+		</C.Container>
+	);
 };
 
 export default Signup;
