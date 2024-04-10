e4dd36269de8ff45c0e735aad9a6d1ee5823e211
AugusttoDaniel <danielsje7133@gmail.com>
2024-04-10 01:09:59 -0300

    Alterando o Auth
    Alteração no useEffect que verifica o login de usuário

--- src/contexts/auth.js
+++ src/contexts/auth.js
@@ -1,74 +1,76 @@
 import { createContext, useEffect, useState } from "react";
-
+import { jwtDecode } from "jwt-decode";
 export const AuthContext = createContext({});
 
 export const AuthProvider = ({ children }) => {
-  const [user, setUser] = useState();
+	const [user, setUser] = useState();
 
-  useEffect(() => {
-    const userToken = localStorage.getItem("user_token");
-    const usersStorage = localStorage.getItem("users_bd");
+	useEffect(() => {
+		const userToken = localStorage.getItem("token");
+		if (userToken) {
+			const decoded = jwtDecode(userToken);
+			const user = {
+				email: decoded.email,
+				id: decoded.id,
+			};
+			setUser(user);
+		}
+	}, []);
 
-    if (userToken && usersStorage) {
-      const hasUser = JSON.parse(usersStorage)?.filter(
-        (user) => user.email === JSON.parse(userToken).email
-      );
+	const signin = async (email, password) => {
+		try {
+			const response = await fetch("https://estagio-omega.vercel.app/token/", {
+				// Adjust the URL based on your setup
+				method: "POST",
+				headers: {
+					"Content-Type": "application/json",
+				},
+				body: JSON.stringify({ email, password }),
+			});
 
-      if (hasUser) setUser(hasUser[0]);
-    }
-  }, []);
+			const data = await response.json();
+			if (response.ok) {
+				localStorage.setItem("token", data.token);
+				return data.mensagem;
+			}
+		} catch (error) {
+			console.error("Login error:", error);
+			throw error;
+		}
+	};
 
-  const signin = (email, password) => {
-    const usersStorage = JSON.parse(localStorage.getItem("users_bd"));
+	const signup = (email, password) => {
+		const usersStorage = JSON.parse(localStorage.getItem("users_bd"));
 
-    const hasUser = usersStorage?.filter((user) => user.email === email);
+		const hasUser = usersStorage?.filter((user) => user.email === email);
 
-    if (hasUser?.length) {
-      if (hasUser[0].email === email && hasUser[0].password === password) {
-        const token = Math.random().toString(36).substring(2);
-        localStorage.setItem("user_token", JSON.stringify({ email, token }));
-        setUser({ email, password });
-        return;
-      } else {
-        return "E-mail ou senha incorretos";
-      }
-    } else {
-      return "Usuário não cadastrado";
-    }
-  };
+		if (hasUser?.length) {
+			return "Já tem uma conta com esse E-mail";
+		}
 
-  const signup = (email, password) => {
-    const usersStorage = JSON.parse(localStorage.getItem("users_bd"));
+		let newUser;
 
-    const hasUser = usersStorage?.filter((user) => user.email === email);
+		if (usersStorage) {
+			newUser = [...usersStorage, { email, password }];
+		} else {
+			newUser = [{ email, password }];
+		}
 
-    if (hasUser?.length) {
-      return "Já tem uma conta com esse E-mail";
-    }
+		localStorage.setItem("users_bd", JSON.stringify(newUser));
 
-    let newUser;
+		return;
+	};
 
-    if (usersStorage) {
-      newUser = [...usersStorage, { email, password }];
-    } else {
-      newUser = [{ email, password }];
-    }
+	const signout = () => {
+		setUser(null);
+		localStorage.removeItem("token");
+	};
 
-    localStorage.setItem("users_bd", JSON.stringify(newUser));
-
-    return;
-  };
-
-  const signout = () => {
-    setUser(null);
-    localStorage.removeItem("user_token");
-  };
-
-  return (
-    <AuthContext.Provider
-      value={{ user, signed: !!user, signin, signup, signout }}
-    >
-      {children}
-    </AuthContext.Provider>
-  );
+	return (
+		<AuthContext.Provider
+			value={{ user, signed: !!user, signin, signup, signout }}
+		>
+			{children}
+		</AuthContext.Provider>
+	);
 };
