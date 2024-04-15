6d9ed243b9a875ea7b4b3222f8cdf88cc120e1c6
AugusttoDaniel <danielsje7133@gmail.com>
2024-04-15 18:21:20 -0300

    Update index.js
    Remove o Fragment do Routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,4 +1,3 @@
-import { Fragment } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import useAuth from "../hooks/useAuth";
 import Home from "../pages/Home";
@@ -14,14 +13,12 @@ const Private = ({ Item }) => {
 const RoutesApp = () => {
   return (
     <BrowserRouter>
-      <Fragment>
         <Routes>
           <Route exact path="/home" element={<Private Item={Home} />} />
           <Route path="/" element={<Signin />} />
           <Route exact path="/signup" element={<Signup />} />
           <Route path="*" element={<Signin />} />
         </Routes>
-      </Fragment>
     </BrowserRouter>
   );
 };
